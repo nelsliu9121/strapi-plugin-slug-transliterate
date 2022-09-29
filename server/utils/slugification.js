fccb44809fc99181d0be6293ecab56eb95ab4ee1
Nelson Liu <nelsliu9121@gmail.com>
2022-09-29 18:07:37 +0800

    use transliteration

--- server/utils/slugification.js
+++ server/utils/slugification.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const slugify = require('@sindresorhus/slugify');
+const { transliterate, slugify: slugifyTransliterate } = require('transliteration');
 const slugifyWithCount = slugify.counter();
 
-const toSlug = (string, options) => slugify(string, options);
-const toSlugWithCount = (string, options) => slugifyWithCount(string, options);
+const toSlug = (string, options) => slugifyTransliterate(string, options);
+const toSlugWithCount = (string, options) => slugifyWithCount(transliterate(string), options);
 
 module.exports = {
 	toSlug,
